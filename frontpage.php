00658d66a347d0b6752d84dee0abbd9f4231e665
Mikail Coban <cobanmikail@icloud.com>
2016-01-14 12:49:54 +0100

    Nieuwe Spotify Widget
    Viral en Top 50

--- frontpage.php
+++ frontpage.php
@@ -18,14 +18,12 @@ include("sidebar.php")
 <article>
 <p>Tags, widgets. Content voor de overige pagina's wordt anders (en met een andere padding).</p>
 <div class="twitter">
-<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/RHAPSODY_010/lists/music" data-widget-id="687271695216959489">Tweets from https://twitter.com/RHAPSODY_010/lists/music</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-<div>
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/RHAPSODY_010/lists/latest-music-headlines" data-widget-id="687271695216959489">Tweets from https://twitter.com/RHAPSODY_010/lists/latest-music-headlines</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script><div>
 <div>
 <div>
 <p><b> Spotify's Top 40 </p></b>
-<iframe src="https://embed.spotify.com/?uri=spotify:user:sonymusicnl:playlist:7yhMkuwajPWTpbEfc7y6oJ&theme=white" width="300" height="600" frameborder="0" allowtransparency="true"></iframe>
-</div>
+<iframe src="http://charts.spotify.com/embed?object=tracks&rank=most_streamed&country=global&windowtype=weekly&date=latest" width="300" height="400" scrolling="no" frameborder="0" allowtransparency="true"></iframe></div>
 </article>
 
 <?php
