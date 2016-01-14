96d12b9fb644b20c62ba535bc14c78116f07921f
Laura <xlaurahilhorst@live.nl>
2016-01-14 12:10:40 +0100

    add 'categories'  header to sidebar, improve layout newtopic

--- newtopic.php
+++ newtopic.php
@@ -7,24 +7,37 @@
 
 <body>
 <?php
-include("header.php") //include header content
-?>
-<?php
-include("navbar.php")
-?>
-<?php 
-include("sidebar.php")
+include("header.php"); //include header content
+
+include("navbar.php");
+ 
+include("sidebar.php");
 ?>
 
 <div class="title"><h1>Create a new topic</h1></div>
 <section>
-	<textarea placeholder="Title" rows="1" cols="100"></textarea>
-	<textarea placeholder="Content" rows="30" cols="100"></textarea>
-	<textarea placeholder="Tags" rows="1" cols="100"></textarea>
-	<input type="submit" value="Post">
-	<form action="newtopic.php">
-    	<input type="submit" value="Create new topic">
-    </form>
+	<div id="topictitle">
+		<textarea placeholder="Title" rows="1" cols="100"></textarea>
+	</div>
+	<div class="message_buttons">
+        <input type="button" value="Bold" onclick="javascript:insert('[b]', '[/b]', 'message');" /><!--
+        --><input type="button" value="Italic" onclick="javascript:insert('[i]', '[/i]', 'message');" /><!--
+        --><input type="button" value="Underlined" onclick="javascript:insert('[u]', '[/u]', 'message');" /><!--
+        --><input type="button" value="Image" onclick="javascript:insert('[img]', '[/img]', 'message');" /><!--
+        --><input type="button" value="Link" onclick="javascript:insert('[url]', '[/url]', 'message');" /><!--
+        --><input type="button" value="Left" onclick="javascript:insert('[left]', '[/left]', 'message');" /><!--
+        --><input type="button" value="Center" onclick="javascript:insert('[center]', '[/center]', 'message');" /><!--
+        --><input type="button" value="Right" onclick="javascript:insert('[right]', '[/right]', 'message');" />
+    </div>
+	<div id="topic_content">
+		<textarea placeholder="Content" rows="30" cols="100"></textarea>
+	</div>
+	<div id="topic_tags">
+		<textarea placeholder="Tags" rows="1" cols="100"></textarea>
+	</div>
+	<div class="button">
+		<input type="submit" value="Post">
+	</div>
   </section>
 
 <?php
--- sidebar.php
+++ sidebar.php
@@ -3,6 +3,7 @@
 	<ul>
 		<li><a href="frontpage.php">Home</a></li>
 		<li><a href="mainindex.php">Forum</a></li>
+		<p>Categories</p>
 		<li><a href="about:blank">Artists</a></li>
 		<li><a href="about:blank">Gear</a></li>
 		<li><a href="about:blank">Music</a></li>
