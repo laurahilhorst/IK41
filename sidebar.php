4124030e2c195492770af0f02dab996a6bf195b5
Laura <xlaurahilhorst@live.nl>
2016-01-14 13:32:50 +0100

    gekust

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,14 @@
 
 <aside>
-	<ul>
+	 <div class="sidebar_text"><ul>
 		<li><a href="frontpage.php">Home</a></li>
 		<li><a href="myprofile.php">My Profile</a></li>
 		<li><a href="about.php">About</a></li>
 		<li><a href="mainindex.php">Forum</a></li>
-		<p>Categories</p>
+		<li>Categories</li>
 		<li><a href="about:blank">Artists</a></li>
 		<li><a href="about:blank">Gear</a></li>
 		<li><a href="about:blank">Music</a></li>
 	</ul>
+</div>
 </aside>
