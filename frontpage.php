1010c3ea7fdc776ab76ca2a40f26fb8f5eecc823
Laura <xlaurahilhorst@live.nl>
2016-01-14 12:36:14 +0100

    sidebar improvement

--- frontpage.php
+++ frontpage.php
@@ -16,16 +16,47 @@ include("navbar.php")
 include("sidebar.php")
 ?>
 <article>
-<p>Tags, widgets. Content voor de overige pagina's wordt anders (en met een andere padding).</p>
-<div class="twitter">
-<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/RHAPSODY_010/lists/music" data-widget-id="687271695216959489">Tweets from https://twitter.com/RHAPSODY_010/lists/music</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-<div>
-<div>
-<div>
-<p><b> Spotify's Top 40 </p></b>
-<iframe src="https://embed.spotify.com/?uri=spotify:user:sonymusicnl:playlist:7yhMkuwajPWTpbEfc7y6oJ&theme=white" width="300" height="600" frameborder="0" allowtransparency="true"></iframe>
-</div>
+	<h2>Popular threads by category</h2>
+	<h3>Artists</h3>
+	<ul>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+	</ul>
+	
+	<h3>Gear</h3>
+	<ul>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+	</ul>
+	
+	</h3>Music</h3>
+	<ul>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+		<li>A thread</li>
+	</ul>
+</article>
+
+</article>
+<article>
+	<div class="twitter">
+	<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/RHAPSODY_010/lists/music" data-widget-id="687271695216959489">Tweets from https://twitter.com/RHAPSODY_010/lists/music</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	</div>
+</article>
+<article>
+	<div>
+	<p><b> Spotify's Top 40 </p></b>
+	<iframe src="https://embed.spotify.com/?uri=spotify:user:sonymusicnl:playlist:7yhMkuwajPWTpbEfc7y6oJ&theme=white" width="300" height="600" frameborder="0" allowtransparency="true"></iframe>
+	</div>
 </article>
 
 <?php
