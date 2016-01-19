4a8bf2b9cefc00d995a93c4da52b3b527fc95d13
Laura <xlaurahilhorst@live.nl>
2016-01-19 18:03:15 +0100

    db connection files, php in navbar
    Shit werkt niet?? Als iemand een ingeving krijgt wat betreft php, ga je
    gang

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+try {
+	$db = new PDO('mysql:host=localhost;dbname=rhapsody;charset=utf8','laura','BPzD5rE8');
+} catch(PDOException $ex) {
+	die("We couldn't connect to the database, sorry!");
+}
+
+	
+
+
+
+
+
+
+
+
+?>
--- navbar.php
+++ navbar.php
@@ -1,3 +1,36 @@
+<?php
+
+if (!empty($_POST['username'])) {
+	include("connectdb.php");
+	
+	$success = false;
+	print ('succes false');
+	$stmt = $db->prepare('SELECT 1 FROM user WHERE username=? AND password=?');
+		$stmt->bindValue(1, $_POST["Username"], PDO::PARAM_STR);
+		$stmt->bindValue(2, $_POST["Password"], PDO::PARAM_STR);
+		$stmt->execute();
+
+	$result = $stmt->fetch(PDO::FETCH_NUM);
+	if ($result) {
+		print ('result');
+		if ($result[0] === "1")
+			$success = true;
+			print ('succes true');
+	}
+
+	if ($success) {
+		echo ("Gelukt!");
+	}
+
+	else {
+		echo ("Login failed. Click <a href=\"{$_SERVER['PHP_SELF']}\">here</a> to try again."); }
+	}
+?>
+<!DOCTYPE html>
+<head>
+	<title>Log in</title>
+</head>
+<body>
 <nav>
     <a href="frontpage.php">Home</a>
     <a href="myprofile.php">My Profile</a>
@@ -6,18 +39,17 @@
 			login/signup
 		</a>
 		<div id="loginBox">
-			<form>
-				<fieldset id="loginBox-inputs">
-					<input id="username" type="username" name="Username" placeholder="Username" required>
-					<input id="password" type="password" name="Password" placeholder="Password" required>
-				</fieldset>
-				<fieldset id="loginBox-submit">
-					<input id="submit" type="submit" value="submit">
-					<a id="signupLink" href="signup.php">
+			<form method="post" action="<?php print $_SERVER['PHP_SELF']; ?>">
+				<input id="username" type="username" name="Username" placeholder="Username" required>
+				<input id="password" type="password" name="Password" placeholder="Password" required>
+				<input id="submit" type="submit" name="submit">
+				<a id="signupLink" href="signup.php">
 						Sign Up
-					</a>
-				</fieldset>
+				</a>
+		
 			</form>
 		</div>
 	</button>
 </nav>
+</body>
+</html>
