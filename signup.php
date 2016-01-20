80ef28dfa7df8b59fb5008c9fa8e7856065ce484
Mikail Coban <cobanmikail@icloud.com>
2016-01-20 14:41:12 +0100

    Signup pagina database

--- signup.php
+++ signup.php
@@ -17,181 +17,108 @@ include("sidebar.php")
 ?>
 
 <article>
-<h1>Sign Up for Rhapsody</h1>
+<h1>Sign Up for Rhapsody Membership</h1>
 
-<form id='signup' action='signup.php' method='post'
-	accept-charset='UTF-8'>
-<fieldset>
-<legend>Sign Up</legend>
-Gender: <br><input type="radio" name="gender" value="Male"> Male
+<form id='signup' method='post' action='signup.php' >
+<label for='gender' >Gender: </label>
+<input type="radio" name="gender" value="Male"> Male
 <input type="radio" name="gender" value="Female"> Female
 <input type="radio" name="gender" value="Other"> Other
 <form action="action_page.php">
 <input type='hidden' name='submitted' id='submitted' value='1'/><br><br>
-<label for='name' >Your Full Name: </label>
+<label for='name' >Full Name: </label>
 <input type='text' name='name' id='name' maxlength="50" /><br><br>
-<label for='email' >Email Address:</label>
+<label for='birthdate' >Birthdate: </label>
+<input type='date' name='birthdate' id="Date"><br><br>
+<label for='name' >Country: </label>
+<input type='text' name='country' id='country' maxlength="50" /><br><br>
+<label for='email' >Email:</label>
 <input type='text' name='email' id='email' maxlength="50" /><br><br>
- 
 <label for='username' >Username:</label>
 <input type='text' name='username' id='username' maxlength="50" /><br><br>
- 
 <label for='password' >Password:</label>
 <input type='password' name='password' id='password' maxlength="50" /><br><br>
-
-Birthday: <br><select name="BirthdayDay">
-	<option> - Day - </option>
-	<option value="1">1</option>
-	<option value="2">2</option>
-	<option value="3">3</option>
-	<option value="4">4</option>
-	<option value="5">5</option>
-	<option value="6">6</option>
-	<option value="7">7</option>
-	<option value="8">8</option>
-	<option value="9">9</option>
-	<option value="10">10</option>
-	<option value="11">11</option>
-	<option value="12">12</option>
-	<option value="13">13</option>
-	<option value="14">14</option>
-	<option value="15">15</option>
-	<option value="16">16</option>
-	<option value="17">17</option>
-	<option value="18">18</option>
-	<option value="19">19</option>
-	<option value="20">20</option>
-	<option value="21">21</option>
-	<option value="22">22</option>
-	<option value="23">23</option>
-	<option value="24">24</option>
-	<option value="25">25</option>
-	<option value="26">26</option>
-	<option value="27">27</option>
-	<option value="28">28</option>
-	<option value="29">29</option>
-	<option value="30">30</option>
-	<option value="31">31</option>
-</select>
-
-<select name="BirthdayMonth">
-	<option> - Month - </option>
-	<option value="January">January</option>
-	<option value="Febuary">Febuary</option>
-	<option value="March">March</option>
-	<option value="April">April</option>
-	<option value="May">May</option>
-	<option value="June">June</option>
-	<option value="July">July</option>
-	<option value="August">August</option>
-	<option value="September">September</option>
-	<option value="October">October</option>
-	<option value="November">November</option>
-	<option value="December">December</option>
-</select>
-
-<select name="BirthdayYear">
-	<option> - Year - </option>
-	<option value="2016">2016</option>
-	<option value="2015">2015</option>
-	<option value="2014">2014</option>
-	<option value="2013">2013</option>
-	<option value="2012">2012</option>
-	<option value="2011">2011</option>
-	<option value="2010">2010</option>
-	<option value="2009">2009</option>
-	<option value="2008">2008</option>
-	<option value="2007">2007</option>
-	<option value="2006">2006</option>
-	<option value="2005">2005</option>
-	<option value="2004">2004</option>
-	<option value="2003">2003</option>
-	<option value="2002">2002</option>
-	<option value="2001">2001</option>
-	<option value="2000">2000</option>
-	<option value="1999">1999</option>
-	<option value="1998">1998</option>
-	<option value="1997">1997</option>
-	<option value="1996">1996</option>
-	<option value="1995">1995</option>
-	<option value="1994">1994</option>
-	<option value="1993">1993</option>
-	<option value="1992">1992</option>
-	<option value="1991">1991</option>
-	<option value="1990">1990</option>
-	<option value="1989">1989</option>
-	<option value="1988">1988</option>
-	<option value="1987">1987</option>
-	<option value="1986">1986</option>
-	<option value="1985">1985</option>
-	<option value="1984">1984</option>
-	<option value="1983">1983</option>
-	<option value="1982">1982</option>
-	<option value="1981">1981</option>
-	<option value="1980">1980</option>
-	<option value="1979">1979</option>
-	<option value="1978">1978</option>
-	<option value="1977">1977</option>
-	<option value="1976">1976</option>
-	<option value="1975">1975</option>
-	<option value="1974">1974</option>
-	<option value="1973">1973</option>
-	<option value="1972">1972</option>
-	<option value="1971">1971</option>
-	<option value="1970">1970</option>
-	<option value="1969">1969</option>
-	<option value="1968">1968</option>
-	<option value="1967">1967</option>
-	<option value="1966">1966</option>
-	<option value="1965">1965</option>
-	<option value="1964">1964</option>
-	<option value="1963">1963</option>
-	<option value="1962">1962</option>
-	<option value="1961">1961</option>
-	<option value="1960">1960</option>
-	<option value="1959">1959</option>
-	<option value="1958">1958</option>
-	<option value="1957">1957</option>
-	<option value="1956">1956</option>
-	<option value="1955">1955</option>
-	<option value="1954">1954</option>
-	<option value="1953">1953</option>
-	<option value="1952">1952</option>
-	<option value="1951">1951</option>
-	<option value="1950">1950</option>
-	<option value="1949">1949</option>
-	<option value="1948">1948</option>
-	<option value="1947">1947</option>
-	<option value="1946">1946</option>
-	<option value="1945">1945</option>
-	<option value="1944">1944</option>
-	<option value="1943">1943</option>
-	<option value="1942">1942</option>
-	<option value="1941">1941</option>
-	<option value="1940">1940</option>
-	<option value="1939">1939</option>
-	<option value="1938">1938</option>
-	<option value="1937">1937</option>
-	<option value="1936">1936</option>
-	<option value="1935">1935</option>
-	<option value="1934">1934</option>
-	<option value="1933">1933</option>
-	<option value="1932">1932</option>
-	<option value="1931">1931</option>
-	<option value="1930">1930</option>
-</select>
-<br><br>Music Experience Level: <br><input type="radio" name="Music Experience Level" value="Beginner"> Beginner
-<input type="radio" name="Music Experience Level" value="Intermediate"> Intermediate
-<input type="radio" name="Music Experience Level" value="Expert"> Expert
-<input type="radio" name="Music Experience Level" value="Professional"> Professional <br><br>
-<input type='submit' name='Submit' value='Submit' />
-</fieldset>
+<label for='experience' >Music Experience Level: </label>
+<input type="radio" name="experience" value="Beginner"> Beginner
+<input type="radio" name="experience" value="Intermediate"> Intermediate
+<input type="radio" name="experience" value="Expert"> Expert
+<input type="radio" name="experience" value="Professional"> Professional <br><br>
+<input type='submit' name='submit' value='Submit' />
 </select>
 </form>
 </article>
 
 <?php
+
 include("footer.php")
+
+
 ?>
+</form>
 </body>
 </html>
+<?php
+include("connectdb.php"); 
+
+	if(isset($_POST['submit'])){
+
+	$gender = $_POST['gender'];
+	$name = $_POST['name'];
+	$birthdate= $_POST['birthdate'];
+	$country = $_POST['country'];
+	$email = $_POST['email'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$experience = $_POST['experience'];
+
+	if($gender==''){
+	echo "<script>alert('Please enter gender')</script>";
+	exit();
+	}
+
+	if($name==''){
+	echo "<script>alert('Please enter name')</script>";
+	exit();
+	}
+
+	if($birthdate==''){
+	echo "<script>alert('Please enter birthdate')</script>";
+	exit();
+	}
+
+	if($country==''){
+	echo "<script>alert('Please enter country')</script>";
+	exit();
+	}
+
+	if($email==''){
+	echo "<script>alert('Please enter email')</script>";
+	exit();
+	}
+
+	$check_email = "SELECT * FROM `user` where email='$email'";
+
+	$run = mysql_query($check_email);
+
+	if(mysql_num_rows($run)>0){
+
+	echo "<script>alert('Email $email is already exist, please try another one')
+	</script>";
+	exit();
+	}
+
+	if($username==''){
+	echo "<script>alert('Please enter username')</script>";
+	exit();
+	}
+
+	if($password==''){
+	echo "<script>alert('Please enter password')</script>";
+	exit();
+	}
+
+	if($experience==''){
+	echo "<script>alert('Please enter Music Experience Level')</script>";
+	exit();
+	}
+}
